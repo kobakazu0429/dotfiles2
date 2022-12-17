38a952abae4a844b077f335f3557bdb9686f2b22
kobakazu0429 <kobakazu10@gmail.com>
2022-12-18 01:34:54 +0900

    update

--- README.md
+++ README.md
@@ -3,9 +3,5 @@
 ## install
 
 ```shell
-wget https://dot.kaz.dev/install && install
-```
-
-<!-- ```shell
 bash -c "$(curl -fsSL https://dot.kaz.dev/install)"
-``` -->
+```
--- cloudflare/functions/[command].js
+++ cloudflare/functions/[command].js
@@ -1,11 +1,16 @@
-const BASE = "https://kobakazu0429.github.io/dotfiles2/"
+const FILE_BASE = "https://raw.githubusercontent.com/kobakazu0429/dotfiles2/master/"
+const BINARY_BASE = "https://kobakazu0429.github.io/dotfiles2/"
 
-const join = (path) => {
-  return new URL(path, BASE).toString();
+const joinBinary = (path) => {
+  return new URL(path, BINARY_BASE).toString();
+}
+const joinFile = (path) => {
+  return new URL(path, FILE_BASE).toString();
 }
 
 const commandMap = {
-  "install": join(("install")),
+  "install": joinFile(("install")),
+  "install_x86_64-apple-darwin": joinBinary(("install_x86_64-apple-darwin")),
 };
 
 export const onRequestGet = async (context) => {
