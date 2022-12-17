0ed5e8e63c2de49d19f9ce927fd8e67d6c4ad418
kobakazu0429 <kobakazu10@gmail.com>
2022-12-18 00:57:31 +0900

    update

--- brew/install.ts
+++ brew/install.ts
@@ -1,11 +1,22 @@
 import { join, resolve } from "path";
+import { log } from "../utils/logger.ts";
+import { exec } from "./../utils/exec.ts";
 import { homeDir } from "./../utils/path.ts";
 import { symbolicLink } from "./../utils/symbolicLink.ts";
 
 export const installBrewfile = () => {
-  const result = symbolicLink(
-    resolve(join(Deno.cwd(), "brew", "Brewfile")),
-    resolve(join(homeDir(), ".Brewfile"))
-  );
-  return result;
+  if (
+    !symbolicLink(
+      resolve(join(Deno.cwd(), "brew", "Brewfile")),
+      resolve(join(homeDir(), ".Brewfile"))
+    )
+  ) {
+    log.error("Failed create symbolic link");
+    Deno.exit(1);
+  }
+
+  if (!exec("brew", { args: ["bundle"] })) {
+    log.error("Failed brew install");
+    Deno.exit(1);
+  }
 };
