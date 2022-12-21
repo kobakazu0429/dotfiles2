22176ad2adf3fef2704a136fd6f03101bd10ce1c
kobakazu0429 <kobakazu10@gmail.com>
2022-12-21 19:16:27 +0900

    update

--- modules/ssh/install.ts
+++ modules/ssh/install.ts
@@ -1,4 +1,5 @@
 import { join, resolve } from "path";
+import { dedent } from "ts-dedent";
 import { exec } from "../../utils/exec.ts";
 import { log } from "../../utils/logger.ts";
 import { homeDir } from "./../../utils/path.ts";
@@ -6,11 +7,21 @@ import { homeDir } from "./../../utils/path.ts";
 export default () => {
   const keyNmae = "id_ed25519_github";
   const dest = resolve(join(homeDir(), ".ssh", keyNmae));
+  const config = resolve(join(homeDir(), ".ssh", "config"));
 
   try {
     Deno.statSync(dest);
     log.info("Generated GitHub SSH key");
   } catch {
+    Deno.writeTextFileSync(
+      config,
+      dedent`Host github
+                    HostName github.com
+                    IdentityFile ${dest}
+                    User git
+                    `
+    );
+
     exec("ssh-keygen", {
       args: ["-t", "ed25519", "-f", dest, "-N", ""],
     });
