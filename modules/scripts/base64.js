af2f5a94ec5aaed320a335bb11b01ebaa5c84e60
kobakazu0429 <kobakazu10@gmail.com>
2022-12-22 00:04:37 +0900

    update

--- modules/scripts/base64.js
+++ modules/scripts/base64.js
@@ -1,21 +1,24 @@
+const { spawn } = require("child_process")
+
 function pbcopy(data) {
-  const { stdin } = require("child_process").spawn("pbcopy");
+  const { stdin } = spawn("pbcopy");
   stdin.write(data);
   stdin.end();
 }
-
 const [, , raw] = process.argv;
 
-const url = new URL(raw)
-url.searchParams.forEach(param => {
-  const decoded = Buffer.from(param, "base64").toString("utf8");
-  if (decoded.startsWith("http")) {
-    console.log(decoded);
-    pbcopy(decoded);
-    process.exit(0);
-  }
-});
-
-const decoded = Buffer.from(raw, "base64").toString("utf8");
-console.log(decoded);
-pbcopy(decoded);
+try {
+  const url = new URL(raw)
+  url.searchParams.forEach(param => {
+    const decoded = Buffer.from(param, "base64").toString("utf8");
+    if (decoded.startsWith("http")) {
+      console.log(decoded);
+      pbcopy(decoded);
+      process.exit(0);
+    }
+  });
+} catch {
+  const decoded = Buffer.from(raw, "base64").toString("utf8");
+  console.log(decoded);
+  pbcopy(decoded);
+}
