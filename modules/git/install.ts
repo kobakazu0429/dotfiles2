c8780dacebec6bf5d7e2051094326c2c7bde9eb9
kobakazu0429 <kobakazu10@gmail.com>
2022-12-21 09:57:09 +0900

    update

--- modules/git/install.ts
+++ modules/git/install.ts
@@ -1,5 +1,6 @@
 import { join, resolve } from "path";
 import { symlink } from "../../utils/symlink.ts";
+import { os, type OS } from "./../../utils/os.ts";
 import { homeDir, __dirname } from "./../../utils/path.ts";
 
 export default () => {
@@ -10,4 +11,13 @@ export default () => {
     const to = resolve(join(homeDir(), ".config", "git", file));
     symlink(source, to);
   }
+
+  const diffHighlight: Record<OS, string> = {
+    intel_mac:
+      "/usr/local/share/git-core/contrib/diff-highlight/diff-highlight",
+    m1_mac:
+      "/opt/homebrew/share/git-core/contrib/diff-highlight/diff-highlight ",
+    ubuntu: "/usr/share/doc/git/contrib/diff-highlight/diff-highlight",
+  };
+  symlink(diffHighlight[os()], "/usr/local/bin/diff-highlight");
 };
