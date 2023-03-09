e451db9ff1b5b438bf017ea3b2712b3f32221872
kobakazu0429 <kobakazu10@gmail.com>
2023-03-09 23:44:12 +0900

    update

--- modules/scripts/install.ts
+++ modules/scripts/install.ts
@@ -1,6 +1,12 @@
 import { exec } from "./../../utils/exec.ts";
 
-export default () => {
-  const packages = ["zx"];
-  exec("yarn", { args: ["global", "add", ...packages] });
+export default {
+  name: "scripts",
+
+  install: () => {
+    const packages = ["zx"];
+    exec("yarn", { args: ["global", "add", ...packages] });
+  },
+
+  update: () => {},
 };
---
+++ modules/zsh/mod.ts
@@ -0,0 +1,23 @@
+import { join, resolve } from "path";
+import { symlink } from "../../utils/symlink.ts";
+import { __dirname, XDG_CONFIG_HOME } from "./../../utils/path.ts";
+
+export default {
+  name: "zsh",
+
+  install: () => {
+    const files = [
+      [".zshrc", resolve(join(XDG_CONFIG_HOME, "zsh"))],
+      [".zshenv", resolve(join(XDG_CONFIG_HOME, "zsh"))],
+      [".zimrc", resolve(join(XDG_CONFIG_HOME, "zim"))],
+    ];
+
+    for (const [filename, _to] of files) {
+      const source = resolve(join(__dirname(import.meta.url), filename));
+      const to = resolve(join(_to, filename));
+      symlink(source, to);
+    }
+  },
+
+  update: () => {},
+};
