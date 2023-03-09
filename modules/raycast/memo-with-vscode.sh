34a8c191f864e09364732303ccda6c4e8b48ee54
kobakazu0429 <kobakazu10@gmail.com>
2023-03-09 23:35:39 +0900

    update

---
+++ modules/raycast/memo-with-vscode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Memo with VSCode
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📝
+
+VSCODE_CMD=code
+WORKING_DIR="$HOME/Documents/memo";
+DEFAULT_FILE_NAME="$(date -u +%FT%TZ).md"
+
+code "$WORKING_DIR" "${WORKING_DIR}/${DEFAULT_FILE_NAME}"
