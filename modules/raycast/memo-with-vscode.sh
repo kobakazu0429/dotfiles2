5edf496ddaf836d9ecee32c305b57822e209a0df
kobakazu0429 <kobakazu10@gmail.com>
2023-11-17 00:05:01 +0900

    update

--- modules/raycast/memo-with-vscode.sh
+++ modules/raycast/memo-with-vscode.sh
@@ -9,7 +9,11 @@
 # @raycast.icon 📝
 
 VSCODE_CMD=code
-WORKING_DIR="$HOME/Documents/memo";
+WORKING_DIR="$HOME/Documents/memo"
 DEFAULT_FILE_NAME="$(date -u +%FT%TZ).md"
+
+if [ ! -d ディレクトリ ]; then
+  mkdir -p $WORKING_DIR
+fi
 
 code "$WORKING_DIR" "${WORKING_DIR}/${DEFAULT_FILE_NAME}"
