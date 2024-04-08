5749cc4b943bb96af8bc6a80a8f7cab74152ba95
kobakazu0429 <kobakazu10@gmail.com>
2024-04-08 23:05:19 +0900

    update

--- modules/ssh/mod.ts
+++ modules/ssh/mod.ts
@@ -1,22 +1,7 @@
-import { join, resolve, dirname } from "path";
-import { ensureFileSync, ensureDirSync } from "fs";
-import { dedent } from "ts-dedent";
 import { execute } from "../../utils/execute.ts";
 import { log } from "../../utils/logger.ts";
-import { homeDir } from "./../../utils/path.ts";
 import { modular } from "./../../utils/modular.ts";
 import { exitGeneralErrors } from "../../utils/exit.ts";
-
-const keyNmae = "id_ed25519_github";
-const destPath = resolve(join(homeDir(), ".ssh", keyNmae));
-const configPath = resolve(join(homeDir(), ".ssh", "config"));
-
-const config = dedent`
-Host github
-  HostName github.com
-  IdentityFile ${destPath}
-  User git
-`;
 
 const checkSSHConnection = () => {
   const { stderrString } = execute("ssh", "-T", "git@github.com", {
@@ -44,25 +29,8 @@ export default modular({
       return;
     }
 
-    try {
-      Deno.statSync(destPath);
-      log.info("Already generated GitHub SSH key.");
-    } catch {
-      ensureDirSync(dirname(configPath));
-      ensureFileSync(configPath);
-      Deno.writeTextFileSync(configPath, config);
-
-      execute("ssh-keygen", "-t", "ed25519", "-f", destPath, "-N", "");
-
-      log.info("Need your action: add your new key");
-      log.info("https://github.com/settings/ssh/new");
-      log.info(Deno.readTextFileSync(destPath + ".pub"));
-      exitGeneralErrors();
-    }
-
-    if (!checkSSHConnection()) {
-      exitGeneralErrors();
-    }
+    log.info("Check 1Password");
+    exitGeneralErrors();
   },
 
   update: () => {},
