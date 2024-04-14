5b2107f70fee18be4de8ec4e2956dd54855a2ea0
kobakazu0429 <kobakazu10@gmail.com>
2024-04-14 12:23:59 +0900

    update

--- cloudflare/functions/[command].js
+++ cloudflare/functions/[command].js
@@ -1,20 +1,22 @@
-const FILE_BASE = "https://raw.githubusercontent.com/kobakazu0429/dotfiles2/master/"
-const BINARY_BASE = "https://kobakazu0429.github.io/dotfiles2/"
+const FILE_BASE =
+  "https://raw.githubusercontent.com/kobakazu0429/dotfiles2/master/";
+const BINARY_BASE = "https://kobakazu0429.github.io/dotfiles2/";
 
 const joinBinary = (path) => {
   return new URL(path, BINARY_BASE).toString();
-}
+};
 const joinFile = (path) => {
   return new URL(path, FILE_BASE).toString();
-}
+};
 
 const commandMap = {
-  "install": joinFile(("install")),
-  "install_x86_64-apple-darwin": joinBinary(("install_x86_64-apple-darwin")),
+  install: joinFile("install"),
+  "install_x86_64-apple-darwin": joinBinary("install_x86_64-apple-darwin"),
+  "install_aarch64-apple-darwin": joinBinary("install_aarch64-apple-darwin"),
 };
 
 export const onRequestGet = async (context) => {
-  const { command } = context.params
+  const { command } = context.params;
   const url = commandMap[command];
   if (!url) return new Response("Not Found");
   const res = await fetch(url);
